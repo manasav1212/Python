cbcd86288a3180fbf46b4f7d8781759670563742
manasav1212 <manasav@unm.edu>
2025-09-22 17:41:10 -0600

    two-sum

---
+++ LeetCode/two-sum.py
@@ -0,0 +1,14 @@
+# Problem: https://leetcode.com/problems/two-sum/
+# Difficulty: Easy
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = dict()
+        for i in range(len(nums)):
+            comp = target - nums[i]
+            if comp in d:
+                return [d[comp], i]
+            d[nums[i]] = i
+        
+            
+        
